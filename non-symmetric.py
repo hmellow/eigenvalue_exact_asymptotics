b3bb1677092cc02ada62adedf8fb3bb49db8093b
hmellow <harry@mellownet.com>
2025-11-18 15:13:32 -0600

    bernoulli entries

--- non-symmetric.py
+++ non-symmetric.py
@@ -16,7 +16,12 @@ def generate_gaussian_random_matrix(p, mean, variance):
     A = np.random.normal(mean, variance, size=(p, p))
     return A
 
+
 def generate_scaled_binary_random_matrix(p):
+    A = np.random.binomial(n=1, p=0.5, size=(p, p))  # Bernoulli trials with p=0.5
+    A = (A * 2) - 1  # {0, 1} -> {-1, 1}
+    A = A / np.sqrt(p)  # {-1, 1} -> {1/-sqrt(p), 1/sqrt(p)}
+    return A
 
 
 def plot_eigenvalue_histogram(A, gamma, variance):
@@ -24,7 +29,10 @@ def plot_eigenvalue_histogram(A, gamma, variance):
     p = A.shape[0]
 
     plt.figure()
-    plt.title("ESD of non-symmetric random matrix - $\\mathcal{N}(0, 1/p)$ case")
+    # plt.title("ESD of non-symmetric random matrix - $\\mathcal{N}(0, 1/p)$ case")
+    plt.title(
+        "ESD of non-symmetric random matrix - $\\mathbb{P}(A_{ij}=\\frac{1}{\\sqrt{p}})=\\mathbb{P}(A_{ij}=\\frac{-1}{\\sqrt{p}})=\\frac{1}{2}$ case"
+    )
     plt.xlabel("Real")
     plt.ylabel("Imaginary")
     plt.plot(
@@ -57,7 +65,8 @@ gamma = 1  # Scaling
 
 
 def main():
-    A = generate_gaussian_random_matrix(p, mean=0, variance=np.sqrt(1 / p**gamma))
+    # A = generate_gaussian_random_matrix(p, mean=0, variance=np.sqrt(1 / p**gamma))
+    A = generate_scaled_binary_random_matrix(p)
     plot_eigenvalue_histogram(A, gamma, np.sqrt(1 / p**gamma))
 
 
